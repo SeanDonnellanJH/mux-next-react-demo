3b5e8c2711c39d9793677655562fbcb9ce3ff518
Sean Donnellan <donnese@macwhhx3yj5wr.prd.manulifeusa.com>
2024-06-25 14:18:19 -0400

    Adds show/hide func with buttons

--- src/app/demo/page.js
+++ src/app/demo/page.js
@@ -6,12 +6,16 @@ import { Button, H1, P, Stack } from '@manulife/mux';
 const Demo = () => {
   const [showText, setShowText] = useState(false);
 
+  const handleOnClick = (value) => {
+    setShowText(value);
+  };
+
   return (
     <Stack align="flex-start">
       <H1>Demo</H1>
       {showText && <P>Content for Demo goes here.</P>}
-      <Button>Show</Button>
-      <Button>Hide</Button>
+      <Button onClick={() => handleOnClick(true)}>Show</Button>
+      <Button onClick={() => handleOnClick(false)}>Hide</Button>
     </Stack>
   );
 };
