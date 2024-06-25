22274a04d4112d9ebdde53a2934dd76e2d7ddf79
Sean Donnellan <donnese@macwhhx3yj5wr.prd.manulifeusa.com>
2024-06-25 14:09:24 -0400

    Introduce state, hide text

--- src/app/demo/page.js
+++ src/app/demo/page.js
@@ -1,14 +1,19 @@
 'use client';
 
+import { useState } from 'react';
 import { Button, H1, P, Stack } from '@manulife/mux';
 
-const Demo = () => (
-  <Stack align="flex-start">
-    <H1>Demo</H1>
-    <P>Content for Demo goes here.</P>
-    <Button>Show</Button>
-    <Button>Hide</Button>
-  </Stack>
-);
+const Demo = () => {
+  const [showText, setShowText] = useState(false);
+
+  return (
+    <Stack align="flex-start">
+      <H1>Demo</H1>
+      {showText && <P>Content for Demo goes here.</P>}
+      <Button>Show</Button>
+      <Button>Hide</Button>
+    </Stack>
+  );
+};
 
 export default Demo;
