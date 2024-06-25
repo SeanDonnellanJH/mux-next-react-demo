820a37569e55ea368a7885ebfc50f3354b3411a1
Sean Donnellan <donnese@macwhhx3yj5wr.prd.manulifeusa.com>
2024-06-25 13:59:00 -0400

    Setup step 1 components, add two buttons to Demo

--- src/app/demo/page.js
+++ src/app/demo/page.js
@@ -1,11 +1,13 @@
 'use client';
 
-import { H1, P, Stack } from '@manulife/mux';
+import { Button, H1, P, Stack } from '@manulife/mux';
 
 const Demo = () => (
   <Stack align="flex-start">
     <H1>Demo</H1>
     <P>Content for Demo goes here.</P>
+    <Button>Show</Button>
+    <Button>Hide</Button>
   </Stack>
 );
 
