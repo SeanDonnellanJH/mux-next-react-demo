24deb946244dd56ae897989af8b719536ba1d320
Sean Donnellan <donnese@macwhhx3yj5wr.prd.manulifeusa.com>
2024-10-21 10:15:56 -0400

    useEffect on Page Load

--- src/app/demo/page.js
+++ src/app/demo/page.js
@@ -1,19 +1,30 @@
 'use client';
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Button, H1, P, Stack } from '@manulife/mux';
 
 const Demo = () => {
+  const [text, setText] = useState('');
   const [showText, setShowText] = useState(false);
 
   const handleOnClick = (value) => {
+    if (value) {
+      setText('You clicked the Show button.');
+    } else {
+      setText('');
+    }
     setShowText(value);
   };
+
+  useEffect(() => {
+    setText('page loaded');
+    setShowText(true);
+  }, []);
 
   return (
     <Stack align="flex-start">
       <H1>Demo</H1>
-      {showText && <P>Content for Demo goes here.</P>}
+      {showText && <P>{text}</P>}
       <Button onClick={() => handleOnClick(true)} disabled={showText}>
         Show
       </Button>
